8c91f25bc5124e71c5b406507ff6964ee3424aa3
Olivier Halligon <olivier.halligon@automattic.com>
2021-03-30 17:19:23 +0200

    Cleanup script

--- tools/update-translations.rb
+++ tools/update-translations.rb
@@ -4,34 +4,14 @@ require 'fileutils'
 require 'open-uri'
 require 'uri'
 
-
 LANG_FILE=File.join(__dir__, 'exported-language-codes.csv')
 RESDIR=File.join(__dir__, '..', 'WooCommerce', 'src', 'main', 'res')
 LANGUAGE_DEF_FILE=File.join(RESDIR, 'values', 'available_languages.xml')
 GLOTPRESS_PROJECT_URL='https://translate.wordpress.com/projects/woocommerce/woocommerce-android'
 
 # Array<[GlotPress code, Android code, Locale name]>
-language_map = File.readlines(LANG_FILE)
-  .reject { |l| l.strip.empty? }
-  .map { |l| l.split(',') }
+language_map = File.readlines(LANG_FILE).reject { |l| l.strip.empty? }.map { |l| l.split(',') }
 
-# Download files
-language_map.each do |gp_code, android_code, _|
-  next if gp_code == 'en-us' # en-us is our base locale, no translations to download for it!
-
-  puts "Downloading translations for '#{android_code}' from GlotPress (#{gp_code})..."
-  lang_dir = File.join(RESDIR, "values-#{android_code}")
-  uri = URI.parse("#{GLOTPRESS_PROJECT_URL}/#{gp_code}/default/export-translations?filters[status]=current&format=android")
-  begin
-    body = uri.read.force_encoding('UTF-8')
-    strings_xml = body.gsub('...','…').gsub("\t", '    ')
-    FileUtils.mkdir(lang_dir) unless Dir.exist?(lang_dir)
-    File.write(File.join(lang_dir, 'strings.xml'), strings_xml)
-  rescue StandardError => e
-    puts "Error downloading #{gp_code} - #{response.code}: #{response.message}"
-    FileUtils.rm_rf(File.join(RESDIR, "values-#{android_code}"))
-  end
-end
 
 # Language definitions resource file
 lang_list = language_map.map { |_, android_code, _| "<item>#{android_code.gsub('-r','_')}</item>" }
@@ -45,3 +25,22 @@ langs_list_xml = <<~XML
   </resources>
 XML
 File.write(LANGUAGE_DEF_FILE, langs_list_xml)
+
+
+# Download translations from GlotPress
+language_map.each do |gp_code, android_code, _|
+  next if gp_code == 'en-us' # en-us is our base locale, no translations to download for it!
+
+  puts "Downloading translations for '#{android_code}' from GlotPress (#{gp_code})..."
+  lang_dir = File.join(RESDIR, "values-#{android_code}")
+  uri = URI.parse("#{GLOTPRESS_PROJECT_URL}/#{gp_code}/default/export-translations?filters[status]=current&format=android")
+  begin
+    body = uri.read.force_encoding('UTF-8')
+    strings_xml = body.gsub('...', '…').gsub("\t", '    ')
+    FileUtils.mkdir(lang_dir) unless Dir.exist?(lang_dir)
+    File.write(File.join(lang_dir, 'strings.xml'), strings_xml)
+  rescue StandardError => e
+    puts "Error downloading #{gp_code} - #{response.code}: #{response.message}"
+    FileUtils.rm_rf(File.join(RESDIR, "values-#{android_code}"))
+  end
+end
